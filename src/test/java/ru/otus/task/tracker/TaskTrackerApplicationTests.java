5c9b42d3ea538220d4fdde55540a6a148b3f91b2
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2025-03-10 12:34:21 +0300

    init commit

---
+++ src/test/java/ru/otus/task/tracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.task.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
