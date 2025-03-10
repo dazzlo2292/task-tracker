cb776a83fc21968ddcb2daa80b8cbc0765f13113
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2025-03-10 12:33:56 +0300

    init commit

---
+++ src/main/java/ru/otus/task/tracker/TaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.task.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
